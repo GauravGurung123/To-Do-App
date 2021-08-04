61cb083de976548a00c4b4ee086cb79eff57b212
Gaurav <gauravgurung065@gmail.com>
2021-08-04 07:29:51 +0545

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './MyComponents/Header'
+import { Todos } from './MyComponents/Todos'
+import { Footer } from './MyComponents/Footer'
+import { AddTodo } from './MyComponents/AddTodo'
+import React, { useState } from 'react'
 
 function App() {
+  const onDelete = (todo) => {
+    console.log('i am onDelete of todo', todo)
+    // Deleting this way in react does not work
+    //let index = todos.indexOf(todo);
+    //todos.splice(index, 1);
+
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo
+      })
+    )
+  }
+  const addTodo = (title, desc) => {
+    console.log('adding..', title, desc)
+
+    let sno = todos[todos.length - 1].sno + 1
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo])
+  }
+
+  const [todos, setTodos] = useState([
+    {
+      sno: 1,
+      title: 'Go to the market',
+      desc: 'You need to go to the market to get this job done',
+    },
+    {
+      sno: 2,
+      title: 'Go to the mall',
+      desc: 'You need to go to the mall to get this job done',
+    },
+    {
+      sno: 3,
+      title: 'Go to the store',
+      desc: 'You need to go to the store to get this job done',
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header title='My Todos List' searchBar={false} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
